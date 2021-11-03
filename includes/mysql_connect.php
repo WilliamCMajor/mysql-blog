f9f123c1b4447c3aa0e544e61fe79f161b5652fa
WilliamCMajor <she8@nait.ca>
2021-11-02 20:06:32 -0600

    deploy on heroku

--- includes/mysql_connect.php
+++ includes/mysql_connect.php
@@ -1,7 +1,18 @@
 <?php
 
 // Connect to DB: Since all files depend on this, this will be included in our header, which is then included in all files.
-$con = mysqli_connect("localhost", "she8","ZoWa89pmsu9qD5o","she8_lab6blog");
+//$con = mysqli_connect("localhost", "she8","ZoWa89pmsu9qD5o","she8_lab6blog");
+
+// development connection Heroku ClearDB
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$con = new mysqli($server, $username, $password, $db);
+
 
 // Check connection
 if (mysqli_connect_errno()) {
@@ -21,7 +32,7 @@ if (mysqli_connect_errno()) {
 // 	We use a constant (basically the same as a variable (stores a value)) to set the Base URL. We will use this to resolve all links as absolute URL's instead of relative URL's. If we do this correct, then we will not have any troubles pathing to all our files.
   // Make sure the path starts with http:// and ends with the trailing slash
   // If you copy/paste this script into other projects and fail to change this, then you may have links going back to the former project. Copy/Paste/FAIL !
-  define("BASE_URL", "http://she8.dmitstudent.ca/dmit2025/lab6-blog/");
+  define("BASE_URL", "https://mysql-blog-william.herokuapp.com/");
   
 
 
